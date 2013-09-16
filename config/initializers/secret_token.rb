9635da38f3bce3ddc180a5e3047fdbe755e4e8ef
Chris McNair <chrismcnair83@gmail.com>
2013-09-16 11:48:22 -0500

    Initial commit for pinecone project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinecone::Application.config.secret_key_base = '54eb4768153dfb4f4818cd8d8bb1a1f00b2b85bf30019f355e4811caf71d62448705ae5b8546cec4aa7163d1f0831e1e5d4b7e5dd49cddfb2287ffc1e39f4393'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinecone::Application.config.session_store :cookie_store, key: '_pinecone_session'
