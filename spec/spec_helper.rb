ee0889768840a7322644dd5e730f046250ac1fc7
Chris McNair <chrismcnair83@gmail.com>
2013-09-16 11:54:35 -0500

    Include capybara configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Needed to use capybara in rspec tests
+  config.include Capybara::DSL
 end
