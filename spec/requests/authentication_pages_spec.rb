a5ac9eef4dd0f53acfc269969882f86078ddbd86
Chris McNair <chrismcnair83@gmail.com>
2013-10-10 21:00:53 -0500

    Add flash for failed signin

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,7 +18,12 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error', test: 'Invalid') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
     describe "with valid information" do
