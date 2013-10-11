21697e3892576fffa2ef9f78a82b76c690ad514a
Chris McNair <chrismcnair83@gmail.com>
2013-10-10 22:06:18 -0500

    Add respec test for flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password'
       render 'new'
