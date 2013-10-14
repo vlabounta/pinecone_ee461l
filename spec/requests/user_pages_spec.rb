c99f78ffe05263afc38ad10fca9203f964b49c58
Chris McNair <chrismcnair83@gmail.com>
2013-10-14 10:33:23 -0500

    Add User edit functionality. User can now visit his edit page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,4 +52,21 @@ describe "UserPages" do
       end
     end
   end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update") }
+      it { should have_title("Edit user") }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+  end
+
 end
