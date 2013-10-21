bcc8fd685910981c8c0aaa626abc2f627b1f8c8d
Chris McNair <chrismcnair83@gmail.com>
2013-10-20 20:42:00 -0500

    Add contextio gem to Gemfile. Attempt to display email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    contextio = ContextIO.new('s4gvms7r', 'Y1ujJ46crM1jFpdU')
+    @account = contextio.accounts.where(email: 'chrismcnair83@gmail.com').first
   end
 
   def help
