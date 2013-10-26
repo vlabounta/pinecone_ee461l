0971bf45fe4b254eeea7bb3facad1a8c9e6de766
Chris McNair <chrismcnair83@gmail.com>
2013-10-25 21:54:22 -0500

    Fix error in static_pages#home when no user is signed in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @user = User.find(current_user)
+    if signed_in?
+      @user = User.find(current_user)
+    end
   end
 
   def help
