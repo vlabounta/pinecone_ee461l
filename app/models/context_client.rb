1172ba5601bfb03c0cac0bb411abac638acedc3e
Chris McNair <chrismcnair83@gmail.com>
2013-11-04 22:42:02 -0600

    Add method for showing recent email. Modify _mail partial to test.

--- app/models/context_client.rb
+++ app/models/context_client.rb
@@ -5,19 +5,26 @@ class ContextClient
  
   attr_accessor :api_handle, :account_id
 
-  def initialize(attributes = {})
-    handle = ContextIO.new(attributes[:key], attributes[:secret])
-    self.send(:api_handle=, handle)
-    self.send(:account_id=, attributes[:account_id]) unless attributes[:account_id].nil?
+  def initialize(account_id, key = 's4gvms7r', secret = 'Y1ujJ46crM1jFpdU')
+    handle = ContextIO.new(key, secret)
+    send(:api_handle=, handle)
+    if account_id.nil?
+      send(:account_id=, account_id) 
+    else
+      send(:account_id=, account_id)
+    end
   end
  
   def persisted?
     false
   end
  
-  def get_last_five_subjects
-    account = api_handle.accounts[account_id]
-    
+  def most_recent_messages(limit = 5)
+    unless self.account_id.nil?
+      account = self.api_handle.accounts[self.account_id]
+      Rails.logger.debug "account object: #{account}"
+      @messages = account.messages.where(limit: 5)
+    end
   end
  
 end
