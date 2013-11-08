2602068f58aba6e4d5476a48e2614de3bfe30df6
Chris McNair <chrismcnair83@gmail.com>
2013-11-08 10:25:05 -0600

    Add foreign key to Task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108154259) do
+ActiveRecord::Schema.define(version: 20131108162204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20131108154259) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "workflow_id"
   end
 
   create_table "users", force: true do |t|
