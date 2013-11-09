981c860d81df311442fe12d1ea0a78d504b5a1db
Chris McNair <chrismcnair83@gmail.com>
2013-11-09 16:05:40 -0600

    Add to create action

--- app/controllers/workflow_controller.rb
+++ app/controllers/workflow_controller.rb
@@ -5,9 +5,18 @@ class WorkflowController < ApplicationController
 
   def create
     @workflow = Workflow.new(workflow_params)
+    if @workflow.save
+      flash[:success] = "#{@workflow.title} workflow successfully created!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    Workflow.find(params[:id]).destroy
+    flash[:success] = "Workflow deleted"
+    redirect_to root_path
   end
 
   def update
