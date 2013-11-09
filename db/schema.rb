0de58ce636567913f25cdc27ff0bd574ddea91c7
Chris McNair <chrismcnair83@gmail.com>
2013-11-09 13:06:55 -0600

    Add user_id foreign key to workflow

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109183227) do
+ActiveRecord::Schema.define(version: 20131109190341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20131109183227) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
