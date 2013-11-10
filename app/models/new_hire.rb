cad83925f2301526621a6b3db4638c83a0d5d82c
Chris McNair <chrismcnair83@gmail.com>
2013-11-09 22:51:26 -0600

    Add validations and before filters for NewHire name and password

--- app/models/new_hire.rb
+++ app/models/new_hire.rb
@@ -1,4 +1,11 @@
 class NewHire < ActiveRecord::Base
   has_many :workflows, dependent: :destroy
   belongs_to :user
+
+  before_save { email.downcase! }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
