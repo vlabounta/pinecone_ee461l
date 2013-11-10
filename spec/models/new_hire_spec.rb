5c5117df35d7ffa1713aa354d4fd766d06262740
Chris McNair <chrismcnair83@gmail.com>
2013-11-09 19:17:02 -0600

    Add rspec tests for validating workflow-new_hire relationship

--- spec/models/new_hire_spec.rb
+++ spec/models/new_hire_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe NewHire do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:new_hire) { FactoryGirl.create(:new_hire) }
+  before do
+    @new_hire = new_hire
+  end
+
+  subject { @new_hire }
+
+  it { should respond_to(:workflows) }
+
 end
--- spec/models/workflow_spec.rb
+++ spec/models/workflow_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe Workflow do
   let(:user) { FactoryGirl.create(:user) }
+  let(:new_hire) { FactoryGirl.create(:new_hire) }
+
   before do
     @workflow = user.workflows.build
+    @workflow.new_hire = new_hire
   end
 
   subject { @workflow }
@@ -11,5 +14,7 @@ describe Workflow do
   it { should respond_to(:tasks) }
   it { should respond_to(:user_id) }
   it { should respond_to(:user) }
+  it { should respond_to(:new_hire_id) }
+  it { should respond_to(:new_hire) }
 
 end
