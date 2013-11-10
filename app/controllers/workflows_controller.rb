6458f007099d11bdca3b34028bbb0075beb8af0c
Chris McNair <chrismcnair83@gmail.com>
2013-11-10 00:41:54 -0600

    Add ability to create tasks in a workflow

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -7,6 +7,15 @@ class WorkflowsController < ApplicationController
     @workflow = current_user.workflows.build(workflow_params)
     @workflow.author_id = current_user.id
     @workflow.new_hire = NewHire.find_by(email: params[:workflow][:goon_email])
+
+    tasks = params[:workflow][:tasks]
+    tasks.each do |task_title|
+      task = Task.new(title: task_title)
+      if task.save
+        @workflow.tasks << task
+      end
+    end
+
     if @workflow.save
       flash[:success] = "#{@workflow.title} workflow successfully created!"
       redirect_to root_url
